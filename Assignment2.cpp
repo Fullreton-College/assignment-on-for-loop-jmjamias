b29b7eed2870119e625d919060cba2231900e6ab
Jamie Jamias <jamiemanabatjamias@gmail.com>
2026-02-27 10:23:17 -0800

    Assignment 2 Complete

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main() {
+   double sales[5];
+// Loop 1: collect input
+   for (int i = 0; i<5; i++) {
+      cout<< "Enter today's sales for store " << (i + 1) << ":"; // since i starts at 0, add 1 so it displays 1,2,3,4,5
+      cin>> sales[i]; //stores what the user types into that slot of the array
+   }   
+   //Print header
+   cout<< "\nSales Bar Chart (Each * = $100)\n";
 
-   /* Type your code here. */
+   // Loop 2: display chart
+   for (int i = 0; i<5; i++) {
+      int stars = sales [i] / 100;
+      cout<< "Store " << (i+1) << ": ";
+      for (int j = 0; j < stars; j++) { //Nested loop to print asterisks
+         cout<< "*";
+      }
+      cout<< endl;
+   }
+
 
    return 0;
 }
+
